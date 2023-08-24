7a4633adad55ba19cfcca975df2a0b7440c169b9
Nuclear_Fusion <81024682+Fusion2010@users.noreply.github.com>
2023-08-24 17:58:38 +0100

    Delete generators directory

--- generators/QP_convertor.m
+++
@@ -1,6 +0,0 @@
-function [c, Q, A, b] = QP_convertor(m, n, C, d, epsilon)
-    c = [d; epsilon * ones(n, 1); epsilon * ones(n, 1)]; 
-    Q = sparse([eye(m, m), zeros(m, 2 * n); zeros(2 * n, m + 2 * n)]); 
-    A = sparse([-eye(m, m), C, -C]); 
-    b = zeros(m, 1); 
-end
